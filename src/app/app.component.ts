30c62a3d55acc631c3721cd773d5957ca7d40068
MohitVermaCoder <verma.mohit95@outlook.com>
2019-12-23 22:57:57 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  // selector: 'app-root',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.sass']
+  selector: 'app-root',
+  template: '<app-login></app-login>',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'GhumoFiro';
+}
